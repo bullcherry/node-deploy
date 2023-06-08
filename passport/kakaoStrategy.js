30cef6a11851534e5e88e4d8af8d5ac4935ac6b5
ATMedia <bullcherry@naver.com>
2023-06-08 19:58:35 +0900

    카카오 이메일 참조 오타 수정
    __json.kakao_account_email -> __json.kakao_account.email

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -1,33 +1,38 @@
-const passport = require('passport');
-const KakaoStrategy = require('passport-kakao').Strategy;
+const passport = require("passport");
+const KakaoStrategy = require("passport-kakao").Strategy;
 
-const { User } = require('../models');
+const { User } = require("../models");
 
 module.exports = () => {
-  passport.use(new KakaoStrategy({
-    clientID: process.env.KAKAO_ID,
-    callbackURL: '/auth/kakao/callback',    
-  }, async (accessToken, refreshToken, profile, done) => {
-    console.log('kakao profile: ', profile);
-    try {
-      const exUser = await User.findOne(
-        { where: { snsId: profile.id, provider: 'kakao' } }
-      );
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        callbackURL: "/auth/kakao/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log("kakao profile: ", profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
 
-      if (exUser) {
-        done(null, exUser);
-      } else {
-        const newUser = await User.create({
-          email: profile._json && profile._json.kakao_account_email,
-          nick: profile.displayName,
-          snsId: profile.id,
-          provider: 'kakao',
-        });
-        done(null, newUser);
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json && profile._json.kakao_account.email,
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+            done(null, newUser);
+          }
+        } catch (err) {
+          console.error(err);
+          done(err);
+        }
       }
-    } catch (err) {
-      console.error(err);
-      done(err);
-    }
-  }));
+    )
+  );
 };
